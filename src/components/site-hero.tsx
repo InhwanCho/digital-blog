e6d14325c67d18b44b0857162b1a2c368d6eeaca
InhwanCho <whdlsghks2@gmail.com>
2024-05-19 17:52:39 +0900

    edit descriptions

--- src/components/site-hero.tsx
+++ src/components/site-hero.tsx
@@ -13,10 +13,10 @@ export default function SiteHero() {
           {siteConfig.title}
         </h1>
         <div className="max-w-[42rem] mx-auto text-slate-800 sm:text-lg text-balance space-y-2">
-          <p>안녕하세요! <span className='text-blue-600/90'>지속적으로 성장하는 개발자</span> 조인환입니다.</p>
+          <p>안녕하세요! <span className='text-blue-700/90'>지속적으로 성장하는 개발자</span> 조인환입니다.</p>
           <p>이 블로그는 개발하면서 학습한 내용을 기록하고 정리하는 공간입니다.</p>
           <p>기획부터 제작, 배포까지 모두 제 손으로 직접 만들었습니다.</p>
-          <p><span className='text-blue-600/90'>Next.js 14 버전</span> 앱 라우터와 <span className='text-blue-600/90'>Velite</span>를 사용하여 정적으로 배포하였습니다.</p>
+          <p><span className='text-blue-700/90'>Next.js 14 버전</span> 앱 라우터와 <span className='text-blue-700/90'>Velite</span>를 사용하여 정적으로 배포하였습니다.</p>
         </div>
         <div className="flex flex-col gap-4 justify-center sm:flex-row">
           <Link
