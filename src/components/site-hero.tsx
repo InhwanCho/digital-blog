66d71a357be944d4d78594c832f1d8eaa79e7133
InhwanCho <whdlsghks2@gmail.com>
2024-06-11 12:12:17 +0900

    fix typo and update posts

--- src/components/site-hero.tsx
+++ src/components/site-hero.tsx
@@ -7,7 +7,7 @@ import { IoLogoGithub } from 'react-icons/io';
 
 export default function SiteHero() {
   return (
-    <section className="space-y-6 pb-8 pt-6 md:pb-12 md:mt-10 lg:py-32">
+    <section className="space-y-6 pb-8 pt-6 md:pb-12 md:mt-10 lg:py-20">
       <div className="container flex flex-col gap-4 text-center ">
         <h1 className="text-3xl sm:text-5xl font-black text-balance sm:pb-4">
           {siteConfig.title}
