93e273711a2e0e5e97841e598df5f5b377ec2fad
InhwanCho <whdlsghks2@gmail.com>
2024-07-03 16:35:06 +0900

    image pathname add

--- next.config.mjs
+++ next.config.mjs
@@ -38,10 +38,12 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "user-images.githubusercontent.com",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "github-production-user-asset-6210df.s3.amazonaws.com",
+        pathname: "/**",
       },
     ],
     formats: ["image/avif", "image/webp"],
